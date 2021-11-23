b7cd41d93167de2202516366390156ceedc45774
unknown <marioalex20007@hotmail.com>
2021-11-23 11:33:08 -0600

    Initial Commit BE

---
+++ src/main/java/mx/tec/web/project/SitatyrChatAppBeApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.web.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitatyrChatAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitatyrChatAppBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/tec/web/project/SitatyrChatAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.tec.web.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitatyrChatAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
