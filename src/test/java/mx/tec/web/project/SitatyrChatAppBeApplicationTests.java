e4968251c47b21461a7caa5f071572db5c53f194
Victor-Guerra <v.e.g.a.2000@hotmail.com>
2021-11-30 20:06:00 -0700

    Added Basic tests

--- src/test/java/mx/tec/web/project/SitatyrChatAppBeApplicationTests.java
+++ src/test/java/mx/tec/web/project/SitatyrChatAppBeApplicationTests.java
@@ -1,9 +1,28 @@
 package mx.tec.web.project;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+@WebMvcTest
 class SitatyrChatAppBeApplicationTests {
+    @Autowired
+    private HttpClient client = HttpClient.newBuilder().build();
 
-
+    @Test
+    public void givenAUserId_1_WhenGetContacts_then2_and_4() throws Exception{
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/contacts/1")).build();
+        HttpResponse<String> res = client.send(req, BodyHandlers.ofString());
+        System.out.println(res.body());
+        assertEquals("", res.body());
+    }
 }
